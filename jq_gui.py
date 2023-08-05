907e50317421579cfd232374576bf866ec616dff
Abu Hassan <46576075+aaabbbuuu@users.noreply.github.com>
2023-08-05 15:42:34 -0400

    Refactoring code to include a main function, and breaking up larger function into smaller functions.

--- jq_gui.py
+++ jq_gui.py
@@ -1,41 +1,66 @@
 import tkinter as tk
-import json
+from tkinter import scrolledtext
 import subprocess
+import json
 
-class JQTool:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("JQ Tool")
-        self.json_label = tk.Label(self.master, text="JSON:")
-        self.json_label.pack()
-        self.json_input = tk.Text(self.master, height=10)
-        self.json_input.pack()
-        self.jq_label = tk.Label(self.master, text="JQ Expression:")
-        self.jq_label.pack()
-        self.jq_input = tk.Entry(self.master, width=50)
-        self.jq_input.pack()
-        self.run_button = tk.Button(self.master, text="Run", command=self.run_jq)
-        self.run_button.pack()
-        self.result_label = tk.Label(self.master, text="Result:")
-        self.result_label.pack()
-        self.result_output = tk.Text(self.master, height=10)
-        self.result_output.pack()
 
-    def run_jq(self):
+class JQProcessorApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("JQ JSON Processor")
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.json_input_text = self.create_labeled_scrolled_text("JSON Input")
+        self.jq_expression_entry = self.create_labeled_entry("JQ Expression")
+        self.apply_button = self.create_apply_button()
+        self.result_text = self.create_labeled_scrolled_text("Result")
+
+    def create_labeled_scrolled_text(self, label):
+        tk.Label(self.root, text=label).pack()
+        text = scrolledtext.ScrolledText(self.root, width=50, height=10)
+        text.pack()
+        return text
+
+    def create_labeled_entry(self, label):
+        tk.Label(self.root, text=label).pack()
+        entry = tk.Entry(self.root, width=50)
+        entry.pack()
+        return entry
+
+    def create_apply_button(self):
+        button = tk.Button(self.root, text="Apply JQ", command=self.apply_jq)
+        button.pack()
+        return button
+
+    def apply_jq(self):
         try:
-            json_input = json.loads(self.json_input.get("1.0", tk.END))
-            jq_expression = self.jq_input.get()
-            result = subprocess.run(["C:\Program Files\JQ\jq.exe", jq_expression], input=json.dumps(json_input), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            if result.returncode == 0:
-                self.result_output.delete("1.0", tk.END)
-                self.result_output.insert(tk.END, result.stdout.strip())
-            else:
-                self.result_output.delete("1.0", tk.END)
-                self.result_output.insert(tk.END, f"Error: {result.stderr.strip()}")
+            json_input = self.json_input_text.get('1.0', 'end-1c')
+            jq_expr = self.jq_expression_entry.get()
+            result = self.process_json(json_input, jq_expr)
+            self.result_text.delete('1.0', tk.END)
+            self.result_text.insert(tk.INSERT, json.dumps(json.loads(result), indent=4))
         except Exception as e:
-            self.result_output.delete("1.0", tk.END)
-            self.result_output.insert(tk.END, f"Error: {e}")
+            self.result_text.delete('1.0', tk.END)
+            self.result_text.insert(tk.INSERT, str(e))
 
-root = tk.Tk()
-app = JQTool(root)
-root.mainloop()
+    def process_json(self, json_input, jq_expr):
+        process = subprocess.Popen(['jq', jq_expr],
+                                    stdin=subprocess.PIPE,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                    text=True)
+        stdout, stderr = process.communicate(json_input)
+        if stderr:
+            raise Exception(stderr)
+        return stdout
+
+
+def main():
+    root = tk.Tk()
+    app = JQProcessorApp(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
